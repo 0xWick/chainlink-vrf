14a655a82680eb35c8dc865f6f6cbbef30e17f26
Wick.py <wickprof@gmail.com>
2022-04-10 22:21:05 +0500

    version-corrected

--- RandomWinnerGame/hardhat/hardhat.config.js
+++ RandomWinnerGame/hardhat/hardhat.config.js
@@ -1,6 +1,5 @@
 require("@nomiclabs/hardhat-waffle");
 require("dotenv").config({ path: ".env" });
-require("@nomiclabs/hardhat-etherscan");
 
 const ALCHEMY_API_KEY_URL = process.env.ALCHEMY_API_KEY_URL;
 
--- RandomWinnerGame/hardhat/scripts/deploy.js
+++ RandomWinnerGame/hardhat/scripts/deploy.js
@@ -5,9 +5,9 @@ const { FEE, VRF_COORDINATOR, LINK_TOKEN, KEY_HASH } = require("../constants");
 
 async function main() {
     /*
-   A ContractFactory in ethers.js is an abstraction used to deploy new smart contracts,
-   so randomWinnerGame here is a factory for instances of our RandomWinnerGame contract.
-   */
+    A ContractFactory in ethers.js is an abstraction used to deploy new smart contracts,
+    so randomWinnerGame here is a factory for instances of our RandomWinnerGame contract.
+    */
     const randomWinnerGame = await ethers.getContractFactory("RandomWinnerGame");
     // deploy the contract
     const deployedRandomWinnerGameContract = await randomWinnerGame.deploy(
@@ -27,7 +27,7 @@ async function main() {
 
     console.log("Sleeping.....");
     // Wait for etherscan to notice that the contract has been deployed
-    await sleep(30000);
+    await sleep(50000);
 
     // Verify the contract after deploying
     await hre.run("verify:verify", {
